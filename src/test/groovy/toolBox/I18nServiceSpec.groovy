e7de5d8f7b0e8b8430ad4001f39994bb9a0d8a4c
JamesChang <james89926282@gmail.com>
2019-12-30 08:42:02 +0800

    新增bootstrap-select 套件

---
+++ src/test/groovy/toolBox/I18nServiceSpec.groovy
@@ -0,0 +1,18 @@
+package toolBox
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class I18nServiceSpec extends Specification implements ServiceUnitTest<I18nService>{
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
